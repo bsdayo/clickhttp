6263c11c6fcd5fe83c9c7614f71a2fe366adf4fa
bsdayo <bsdayo@outlook.com>
2024-06-05 15:51:38 +0800

    feat: initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.FileProviders;
+
+var app = WebApplication.Create(args);
+
+app.Environment.WebRootFileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory());
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.Run();
