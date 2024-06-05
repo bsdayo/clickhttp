70a74f281431c2c045f9a8d464733d5ab48fb270
bsdayo <bsdayo@outlook.com>
2024-06-05 16:23:27 +0800

    feat: redirect to index.html on 404

--- Program.cs
+++ Program.cs
@@ -5,5 +5,6 @@ var app = WebApplication.Create(args);
 app.Environment.WebRootFileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory());
 app.UseDefaultFiles();
 app.UseStaticFiles();
+app.MapFallback(() => Results.Redirect("/index.html"));
 
 app.Run();
